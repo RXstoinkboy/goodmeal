7564c9faf878908075a46bf77a362d596acc1c83
RXstoinkboy <erykludwin@gmail.com>
2019-02-10 23:19:34 +0100

    almost done with layout of specialities

--- src/components/Hero/Specialites/Specialities.js
+++ src/components/Hero/Specialites/Specialities.js
@@ -1,11 +1,12 @@
-import {SpecialitiesWrapper, Photo, Middle} from './SpecialitiesStyle';
+import {SpecialitiesWrapper, Photo, Middle, WrapperBlock, Container} from './SpecialitiesStyle';
 import {Subtitle} from '../HeroStyle';
 import React, { Component } from 'react';
+import speciality1 from '../../../img/speciality1.jpg';
 
 export class Specialities extends Component {
     render() {
         return (
-            <SpecialitiesSection />
+            <SpecialitiesSection></SpecialitiesSection>
         );
     }
 }
@@ -17,11 +18,18 @@ const SpecialitiesSection =props=> {
                 <Subtitle>our specialities</Subtitle>
             </Photo>
             <Middle>
-                Hello from middle
+                <WrapperBlock>
+                    <Container>1</Container>
+                    <Container photo={speciality1}/>
+                    <Container>2</Container>
+                    <Container photo={speciality1}/>
+                    <Container photo={speciality1}/>
+                    <Container>3</Container>
+                    <Container photo={speciality1}/>
+                    <Container>4</Container>
+                </WrapperBlock>
             </Middle>
-            <Photo>
-                Hello from bottom
-            </Photo>
+            <Photo />
         </SpecialitiesWrapper>
     );
 };
--- src/components/Hero/Specialites/SpecialitiesStyle.js
+++ src/components/Hero/Specialites/SpecialitiesStyle.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-import specialitiesBottom from '../../../img/specialitiesBottom.jpg';
 import specialitiesTop from '../../../img/specialitiesTop.jpg';
+import kitchen from '../../../img/kitchen.jpg';
 
 export const Photo = styled.div`
-    background: url(${props => props.top ? specialitiesTop : specialitiesBottom});
+    background: url(${props => props.top ? specialitiesTop : kitchen});
     width: 100vw;
     height: 50vh;
     background-origin: center;
@@ -15,10 +15,15 @@ export const Photo = styled.div`
     justify-content: center;
     align-items: center;
     flex-direction: column;
+
+    @media all and (max-width: 800px){
+        display: ${props => props.top ? 'flex' : 'none'};
+        height: 200px;
+    }
 `
 
 export const Middle = styled.div`
-    background: var(--mainGrad);
+    background: white;
     width: 100vw;
     height: 40vh;
     display: flex;
@@ -26,6 +31,10 @@ export const Middle = styled.div`
     justify-content: center;
     align-items: center;
     flex-direction: column;
+
+    @media all and (max-width: 800px){
+        height: auto
+    }
 `
 
 export const SpecialitiesWrapper = styled.div`
@@ -33,3 +42,39 @@ export const SpecialitiesWrapper = styled.div`
     width: 100vw;  
     background: teal                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 `
+export const Container = styled.div`
+    min-width: 250px;
+    min-height: 250px;
+    ${props => props.photo ? `background-image: url(${props.photo})`: `background: var(--mainGrad)`};
+    background-origin: center;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    
+    @media all and (max-width: 500px){
+        display: ${props => props.photo ? 'none' : 'grid'}
+    }
+`
+
+export const WrapperBlock = styled.div`
+    z-index:2;
+    min-width: 800px;
+    max-width: 1000px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    box-shadow: 0 4px 50px black;
+
+    @media all and (max-width: 800px){
+        grid-template-columns: 1fr 1fr;
+        width: 100%;
+        z-index:1;
+    }
+
+    @media all and (max-width: 500px){
+        grid-template-columns: 1fr;
+    }
+`
+
